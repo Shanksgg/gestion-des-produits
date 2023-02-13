c344a1f0cbbd35aae70edd6428de4a7cad304962
Anass Soukrat <shanks@gmail.com>
2023-02-13 16:22:41 +0100

    Fix Category Submit

--- Artisanal.Web/Program.cs
+++ Artisanal.Web/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddHttpClient<IProductService, ProductService>();
+builder.Services.AddHttpClient<ICategoryService, CategoryService>();
 SD.ProductAPIBase = builder.Configuration["ServiceUrls:ProductAPI"];
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
